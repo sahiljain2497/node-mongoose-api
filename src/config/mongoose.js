ef140a31d50e816008a3e9d8826bd277fd66c1ea
Sahil Jain <sahiljainpkl199724@gmail.com>
2021-07-14 00:07:47 +0530

    Update mongoose.js
    mongoose depr update for index

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -10,7 +10,7 @@ module.exports = {
       if (process.env.NODE_ENV === 'development') {
         mongoose.set('debug', true);
       }
-      mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+      mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
       const db = mongoose.connection;
       db.on('error', () => {
         console.error.bind(console, 'connection error:');
